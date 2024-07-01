dec9d0aed91d544e753666e7842c90ff1e376806
Naresh <nn0887978@gmail.com>
2024-07-01 16:56:44 +0530

    initialpush

---
+++ index.js
@@ -0,0 +1,32 @@
+let Name = 'hello world'
+console.log(Name);
+
+var a=10;
+var b=30;
+console.log(a+b)
+var c=20;
+var d=50;
+console.log(c-d)
+var e=70;
+var f=20;
+console.log(e*f)
+var g=10;
+var h=25;
+console.log(g/h)
+
+
+
+
+var greeter = "hey hi";
+if (true) {
+    let hello = "say Hello instead";
+     console.log(hello);
+}
+console.log(greeter);
+
+let greeting = "say Hi";
+    if (true) {
+        let greeting = "say Hello instead";
+        console.log(greeting); 
+    }
+    console.log(greeting)
